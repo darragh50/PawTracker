2b15f99b9ad8ba0381224443310db4043a017e00
darragh50 <darraghrohan@gmail.com>
2025-04-27 22:02:57 +0100

    Connect firebase again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC67OqWTTe0anekFxKDjYemzjh30sUU7XQ",
+    authDomain: "pawtrack-28065.firebaseapp.com",
+    projectId: "pawtrack-28065",
+    storageBucket: "pawtrack-28065.firebasestorage.app",
+    messagingSenderId: "120451693863",
+    appId: "1:120451693863:web:968a205d83c3a42ac47d02"
+  }
 };
 
 /*
