7c428c473e7ab412d160604ac6eff5d1fff843e1
darragh50 <darraghrohan@gmail.com>
2025-04-27 22:07:16 +0100

    Add angular and configure for mobile development

---
+++ android/app/src/main/java/com/darragh/pawtacknew/MainActivity.java
@@ -0,0 +1,5 @@
+package com.darragh.pawtacknew;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'pawtrack-new',
+  appId: 'com.darragh.pawtacknew',
+  appName: 'PawTrack',
   webDir: 'www'
 };
 
