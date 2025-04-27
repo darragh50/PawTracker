0ee35516249259f532d774f35ff9cc6a5126455c
darragh50 <darraghrohan@gmail.com>
2025-04-27 23:25:40 +0100

    Deploy app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC67OqWTTe0anekFxKDjYemzjh30sUU7XQ",
+    authDomain: "pawtrack-28065.firebaseapp.com",
+    projectId: "pawtrack-28065",
+    storageBucket: "pawtrack-28065.firebasestorage.app",
+    messagingSenderId: "120451693863",
+    appId: "1:120451693863:web:968a205d83c3a42ac47d02"
+  }
 };
+
+
