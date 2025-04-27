16e0949c9da8c0a38d49fcfcf03e6f0078e8e813
darragh50 <darraghrohan@gmail.com>
2025-04-27 22:21:01 +0100

    Dog-detial component, html and style

---
+++ src/app/dogs/dog-detail/dog-detial.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule, NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { PetsService } from '../../services/pets.service';
+
+@Component({
+  selector: 'app-dog-detail',
+  templateUrl: './dog-detail.component.html',
+  styleUrls: ['./dog-detail.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule]
+})
+export class DogDetailComponent implements OnInit {
+  dogId: string = '';
+  dog$: Observable<any>;
+  selectedSegment: string = 'details';
+
+  constructor(
+    private route: ActivatedRoute,
+    private petsService: PetsService,
+    private navCtrl: NavController
+  ) {
+    this.dogId = this.route.snapshot.paramMap.get('id') || '';
+    this.dog$ = this.petsService.getPetById(this.dogId);
+  }
+
+  ngOnInit() {}
+
+  segmentChanged(event: any) {
+    this.selectedSegment = event.detail.value;
+  }
+
+  goBack() {
+    this.navCtrl.back();
+  }
+}
